230c0e36d6420e65a2d6a658cd3019833af1f215
L-Tello <81892001+L-Tello@users.noreply.github.com>
2021-04-13 22:31:16 -0600

    Heroku conf2

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ def principal():
 
 
 if __name__ == '__main__':
-    puerto = int(os.environ.get('PORT', 3000))
-    app.run(host = '0.0.0.0', port = puerto)
+    puerto = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0',port=puerto)
 
 
 
