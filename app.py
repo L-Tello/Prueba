5ddb5f1af87f23bf9d3cfcc753639d3c2bdc04a8
L-Tello <81892001+L-Tello@users.noreply.github.com>
2021-04-13 22:20:15 -0600

    Heroku conf

--- app.py
+++ app.py
@@ -9,12 +9,9 @@ CORS(app)
 
 @app.route('/', methods=['GET'])
 def principal():
-    return {"nombre:" "Ludwin Tello", "carnet":"202010303"}
+    return {"nombre":"Ludwin Tello", "carnet":"202010303"}
 
-@app.route('/hola', methods=['GET'])
-def hola():
-    return "Hola"
-    
+
 if __name__ == '__main__':
     puerto = int(os.environ.get('PORT', 3000))
     app.run(host = '0.0.0.0', port = puerto)
